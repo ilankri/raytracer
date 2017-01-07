999ccfa143df3dc6a38d2695f306e8322544f90d
Idir Lankri <lankri.idir@gmail.com>
2017-01-07 13:40:14 +0100

    beam.ml: implement make and fix some typos

--- src/beam.ml
+++ src/beam.ml
@@ -1,18 +1,23 @@
-let intersect bean obj =
-match obj with
- | Plane(n, r) -> 
-   (
-    let p = Vect.scalprod n beam.source in
-    if (p <= r) then None 
-    else if (Vect.scalprod n beam.direction <=0 ) then None
-    else
-      let t = (r -. (Vect.scalprod n beam.source)) /. (Vect.scalprod n beam.direction) in 
-        Vect.add beam.source (shift t beam.direction)
-   )
- | _ -> failwith "Intersection of this object is not yet implemented"
-
-(*type t = {
-  source : Vect.t (* source of light from camera *)
+type t = {
+  source : Vect.t; (* source of light from camera *)
   direction : Vect.t (* direction of light from camera *)
 }
-*)
+
+let make src dir = { source = src; direction = dir }
+
+let intersect beam = Object.(function
+    | Plane (n, r) ->
+      let p = Vect.scalprod n beam.source in
+      if p <= r then None
+      else if Vect.scalprod n beam.direction <= 0. then None
+      else
+        let t =
+          (r -. (Vect.scalprod n beam.source))
+          /. (Vect.scalprod n beam.direction)
+        in
+        Some (Vect.add beam.source (Vect.shift t beam.direction))
+    | _ -> failwith "Intersection of this object is not yet implemented"
+  )
+
+(* TODO *)
+let trace ray nb_max_reb scene = Color.make 0. 0. 1.
