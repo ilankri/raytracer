6668203bcb2240e1a3b2ff97e9686878da1d02ba
Idir Lankri <lankri.idir@gmail.com>
2017-01-07 13:41:23 +0100

    Update Ray.main function

--- src/ray.ml
+++ src/ray.ml
@@ -1,4 +1,3 @@
-
 let read_scenario f =
   let buf = open_in f in
   let lexbuf = Lexing.from_channel buf in
@@ -10,21 +9,28 @@ let read_scenario f =
     let open Lexing in
     let pos = lexbuf.lex_curr_p in
     Printf.eprintf "File %s, line %d, character %d: parse error\n"
-                  f pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1);
+      f pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1);
     flush stderr;
     close_in buf;
     raise e
 
 let main () =
-  if Array.length Sys.argv = 0 then
+  if Array.length Sys.argv = 1 then
     begin
       print_string "no scenario file given!\n";
       exit 1;
     end;
-  let _ = read_scenario Sys.argv.(1) in
-  print_string "Scenario file correctly read. Stop.\n";
+  let scenario = read_scenario Sys.argv.(1) in
+  let scene = Scene.make scenario 0 in
+  let width = 500 in
+  let height = 250 in
+  let img = Image.make width height scene in
+  Graphics.open_graph (" " ^ string_of_int width ^ "x" ^ string_of_int height);
+  Graphics.draw_image (Image.to_graphics img) 0 0;
+  (* TODO: handle events properly.  *)
+  ignore (Graphics.wait_next_event []);
+  (* print_string "Scenario file correctly read. Stop.\n"; *)
   exit 0
 
 let _ =
   if not !Sys.interactive then main ()
-
