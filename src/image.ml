62a086c6a833a6c776a3a76958693c8b6013485d
Idir Lankri <lankri.idir@gmail.com>
2017-01-08 12:06:58 +0100

    Fix bugs in Image.make
    Rays from camera to screen were wrongly computed.

--- src/image.ml
+++ src/image.ml
@@ -2,21 +2,24 @@ type t = Color.t array array
 
 let make width height scene =
   let img = Array.make_matrix height width Color.black in
-  let cam_dist = Scene.(scene.camera.viewdist) in
   let ray_of_pixel x y =
     let x = float_of_int x in
     let y = float_of_int y in
-    let half_width = float_of_int (width / 2) in
     let screen_point =
-      Vect.make (x -. half_width) y (cam_dist -. half_width *. tan (1. /. 2.))
+      Vect.make
+        (x -. float_of_int (width asr 1))
+        (y -. float_of_int (height asr 1))
+        Scene.(scene.camera.viewdist -.
+               float_of_int width /.
+               ldexp (tan (ldexp scene.camera.angle (-1))) 1)
     in
-    let src = Vect.make 0. 0. cam_dist in
-    let dir = Vect.normalised_diff src screen_point in
+    let src = Vect.make 0. 0. Scene.(scene.camera.viewdist) in
+    let dir = Vect.normalised_diff screen_point src in
     Beam.make src dir
   in
   for i = 0 to height - 1 do
     for j = 0 to width - 1 do
-      img.(i).(j) <- Beam.trace (ray_of_pixel i j) 1 scene;
+      img.(i).(j) <- Beam.trace (ray_of_pixel j i) 1 scene;
     done;
   done;
   img
@@ -27,7 +30,7 @@ let to_graphics img =
   in
   for i = 0 to Array.length img - 1 do
     for j = 0 to Array.length img.(0) - 1 do
-      img_graphics.(i).(j) <- Color.to_graphics (img.(i).(j))
+      img_graphics.(i).(j) <- Color.to_graphics img.(i).(j)
     done;
   done;
   Graphics.make_image img_graphics
