b12df1e14d083d529fde91eef9ba848d69ad18ef
Quincy Hsieh <quincy.tw@gmail.com>
2017-01-08 14:45:10 +0100

    Add color white`

--- src/color.mli
+++ src/color.mli
@@ -8,6 +8,7 @@ val make : float -> float -> float -> t
 
 (* Same as make 0. 0. 0. *)
 val black : t
+val white : t
 
 (* Convert to integer (r,g,b), each component in 0..255 *)
 val to_bytes : t -> int*int*int
